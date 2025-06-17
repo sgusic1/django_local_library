152123c745544cd9a19a649d260212fea92d59fb
Sabina Gušić <sabinagusic3@gmail.com>
2025-06-17 14:59:05 +0200

    Configured Admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+# admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+
+    inlines = [BookInstanceInline]
+admin.site.register(Book, BookAdmin)
+
+# admin.site.register(Author)
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
+#Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+admin.site.register(Genre)
+
+# admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availabilty', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+admin.site.register(Language)
 
 # Register your models here.
