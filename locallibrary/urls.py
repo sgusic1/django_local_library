f97ea448c7d3750217142bb257b198c627deed8f
Sabina Gušić <sabinagusic3@gmail.com>
2025-07-22 13:54:13 +0200

    Add built-in authentication paths under /accounts/

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -27,3 +27,7 @@ urlpatterns = [
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=False)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
