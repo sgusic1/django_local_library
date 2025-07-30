793d7f02040f1587e61a8d06c0da58223828ec64
Sabina Gušić <sabinagusic3@gmail.com>
2025-07-30 18:59:15 +0200

    Define URL for librarian book renewal view

--- catalog/urls.py
+++ catalog/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('author/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    path('borrowed/', views.AllBorrowedBooksListView.as_view(), name='all-borrowed')
+    path('borrowed/', views.AllBorrowedBooksListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian')
 ]
