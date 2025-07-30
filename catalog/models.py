e0359e102577e89529fdd768e9b18e5a54b23c76
Sabina Gušić <sabinagusic3@gmail.com>
2025-07-30 19:36:18 +0200

    Define can_renew permission

--- catalog/models.py
+++ catalog/models.py
@@ -88,7 +88,11 @@ class BookInstance(models.Model):
 
     class Meta: 
         ordering = ['due_back']
-        permissions = (("can_mark_returned", "Set book as returned"),("can_see_all_borrowed_books", "View all borrowed books"))
+        permissions = (
+            ("can_mark_returned", "Set book as returned"),
+            ("can_see_all_borrowed_books", "View all borrowed books"), 
+            ("can_renew", "Renew the due back date")
+        )
 
     def __str__(self):
         """String for representing the Model object."""
