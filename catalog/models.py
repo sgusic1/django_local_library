acee5d3f80bd7b54e0b5d3c5f25d4b661dddc277
Sabina Gušić <sabinagusic3@gmail.com>
2025-08-23 14:07:23 +0200

    Add book creation functionality
    - Added URL pattern for the book create view
    - Implemented the create view for Book
    - Created book_form.html template for rendering the form
    - Updated base_generic.html to include a link to the new view

--- catalog/models.py
+++ catalog/models.py
@@ -42,9 +42,10 @@ class Book(models.Model):
     summary = models.TextField(
         max_length=1000, help_text="Enter a brief description of the book"
     )
-    isbn = models.CharField('ISBN', max_length=13, unique=True, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+    isbn = models.CharField('ISBN', max_length=20, unique=True, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    
 
     def __str__(self):
         """String for representing the Model object."""
