d29b844b158addd24a1284a64baf86030a6f2270
Sabina Gušić <101071742+sgusic1@users.noreply.github.com>
2025-10-15 15:32:46 +0200

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-u3obyv#4n&5686
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['binaa12.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
