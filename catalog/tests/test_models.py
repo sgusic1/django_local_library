bf52d1030bda30e513b8fba932b3f1e26d6f067e
Sabina Gušić <sabinagusic3@gmail.com>
2025-10-18 15:25:56 +0200

    Add unit tests for the Genre Model

--- catalog/tests/test_models.py
+++ catalog/tests/test_models.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from catalog.models import Author
+from catalog.models import Author, Genre
 
 class AuthorModelTest(TestCase):
     @classmethod
@@ -46,3 +46,44 @@ class AuthorModelTest(TestCase):
     def test_get_absolute_url(self):
         author = Author.objects.get(id=1)
         self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
+
+class GenreModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Setting up non-modified object to be used by all test methods
+        Genre.objects.create(name='Science Fiction')
+
+    def test_name_label(self):
+        genre = Genre.objects.get(id=1)
+        field_label = genre._meta.get_field('name').verbose_name
+        self.assertEqual(field_label, 'name')
+
+    def test_name_max_length(self):
+        genre = Genre.objects.get(id=1)
+        max_length = genre._meta.get_field('name').max_length
+        self.assertEqual(max_length, 200)
+
+    def test_name_unique(self):
+        genre = Genre.objects.get(id=1)
+        unique = genre._meta.get_field('name').unique
+        self.assertTrue(unique)
+
+    def test_name_help_text(self):
+        genre = Genre.objects.get(id=1)
+        help_text = genre._meta.get_field('name').help_text
+        self.assertEqual(help_text, "Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
+
+    def test_object_name_is_name(self):
+        genre = Genre.objects.get(id=1)
+        expected_object_name = genre.name
+        self.assertEqual(str(genre), expected_object_name)
+
+    def test_genre_name_case_insensitive_unique(self):
+        # Test that creating a genre with the same name but different case raises an error
+        with self.assertRaises(Exception):
+            Genre.objects.create(name='science fiction')
+
+    def test_genre_name_case_insensitive_unique_different_case(self):
+        # Test that creating a genre with different case but same lower case raises error
+        with self.assertRaises(Exception):
+            Genre.objects.create(name='SCIENCE FICTION')
